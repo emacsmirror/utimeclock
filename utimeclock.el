94ca9d5fa2ab1f04370814ae44b6b3d16d213952
Campbell Barton <ideasman42@gmail.com>
2024-04-20 20:30:09 +1000

    Bump emacs version to 29.1

--- utimeclock.el
+++ utimeclock.el
@@ -7,7 +7,7 @@
 
 ;; URL: https://codeberg.org/ideasman42/emacs-utimeclock
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "29.1"))
 
 ;;; Commentary:
 
@@ -23,22 +23,6 @@
 
 (eval-when-compile
   (require 'subr-x))
-
-
-;; ---------------------------------------------------------------------------
-;; Compatibility
-
-(when (and (version< emacs-version "29.1") (not (and (fboundp 'pos-bol) (fboundp 'pos-eol))))
-  (defun pos-bol (&optional n)
-    "Return the position at the line beginning."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-beginning-position n)))
-  (defun pos-eol (&optional n)
-    "Return the position at the line end."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-end-position n))))
 
 
 ;; ---------------------------------------------------------------------------
